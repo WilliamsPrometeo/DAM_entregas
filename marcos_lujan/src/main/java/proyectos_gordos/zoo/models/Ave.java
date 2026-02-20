ff32125b00508645eb96dbbb1b200939a4d1af4f
Marcoslujan <marcoslujanmiguel112@gmail.com>
2026-02-20 11:32:28 +0100

    Bienvenido al zoo APAPAPONGAMAMOPAPOLUNGA, donde todos sus sueños se hacen realizad. PD: Apruebame William 😘.

--- marcos_lujan/src/main/java/proyectos_gordos/zoo/models/Ave.java
+++ marcos_lujan/src/main/java/proyectos_gordos/zoo/models/Ave.java
@@ -3,37 +3,81 @@ package proyectos_gordos.zoo.models;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Clase Ave que extiende Animal.
+ *
+ * Representa un ave del zoológico con longitud de alas y
+ * si se trata de una rapaz.
+ *
+ * @author Alumno - Marcos Luján Miguel
+ * @version 1.0
+ */
 public class Ave extends Animal {
 
     private int longitud_alas;
     private boolean rapaz;
 
+    /**
+     * Constructor de Ave.
+     *
+     * @param id identificador del ave
+     * @param nombre nombre del ave
+     * @param especie especie del ave
+     * @param raza raza del ave
+     * @param fechaEntrada fecha de entrada al zoológico
+     * @param longitud_alas longitud de las alas en centímetros
+     * @param rapaz true si es rapaz, false en caso contrario
+     */
     public Ave(String id, String nombre, String especie, String raza, LocalDate fechaEntrada, int longitud_alas, boolean rapaz) {
         super(id, nombre, especie, raza, fechaEntrada);
         this.longitud_alas = longitud_alas;
         this.rapaz = rapaz;
     }
 
+    /**
+     * Getter de longitud_alas
+     * @return longitud de las alas en centímetros
+     */
     public int getLongitud_alas() {
         return longitud_alas;
     }
 
+    /**
+     * Setter de longitud_alas
+     * @param longitud_alas longitud de las alas en centímetros
+     */
     public void setLongitud_alas(int longitud_alas) {
         this.longitud_alas = longitud_alas;
     }
 
+    /**
+     * Consulta si el ave es rapaz
+     * @return true si es rapaz, false de lo contrario
+     */
     public boolean isRapaz() {
         return rapaz;
     }
 
+    /**
+     * Setter de la propiedad rapaz
+     * @param rapaz indica si el ave es rapaz
+     */
     public void setRapaz(boolean rapaz) {
         this.rapaz = rapaz;
     }
 
+    /**
+     * Devuelve el tipo de animal.
+     * @return cadena que describe el tipo de animal
+     */
     @Override
     public String getTipoAnimal() {return "";
     }
 
+    /**
+     * Método toString para mostrar los datos del ave.
+     * @return texto con la información formateada del ave
+     */
     @Override
     public String toString() {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
--- marcos_lujan/src/main/java/proyectos_gordos/zoo/models/Mamifero.java
+++ marcos_lujan/src/main/java/proyectos_gordos/zoo/models/Mamifero.java
@@ -3,37 +3,81 @@ package proyectos_gordos.zoo.models;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Clase Mamifero representa animales mamíferos dentro del zoológico.
+ * Contiene información adicional sobre patas y color de pelaje.
+ *
+ * @author Alumno - Marcos Luján Miguel
+ * @version 1.0
+ */
 public class Mamifero extends Animal {
 
+    /** número de patas del mamífero */
     private int numero_patas;
+    /** color del pelaje del mamífero */
     private String color_pelaje;
 
+    /**
+     * Constructor Mamifero
+     *
+     * @param id identificador único del animal
+     * @param nombre nombre común del mamífero
+     * @param especie especie biológica
+     * @param raza raza concreta dentro de la especie
+     * @param fechaEntrada fecha de ingreso al zoológico
+     * @param numero_patas número de patas del mamífero
+     * @param color_pelaje color del pelaje del mamífero
+     */
     public Mamifero(String id, String nombre, String especie, String raza, LocalDate fechaEntrada, int numero_patas, String color_pelaje) {
         super(id, nombre, especie, raza, fechaEntrada);
         this.numero_patas = numero_patas;
         this.color_pelaje = color_pelaje;
     }
 
+    /**
+     * Getter del atributo numero_patas
+     * @return cantidad de patas del mamífero
+     */
     public int getNumero_patas() {
         return numero_patas;
     }
 
+    /**
+     * Setter del atributo numero_patas
+     * @param numero_patas nueva cantidad de patas
+     */
     public void setNumero_patas(int numero_patas) {
         this.numero_patas = numero_patas;
     }
 
+    /**
+     * Getter del atributo color_pelaje
+     * @return color del pelaje
+     */
     public String getColor_pelaje() {
         return color_pelaje;
     }
 
+    /**
+     * Setter del atributo color_pelaje
+     * @param color_pelaje nuevo color del pelaje
+     */
     public void setColor_pelaje(String color_pelaje) {
         this.color_pelaje = color_pelaje;
     }
 
+    /**
+     * Devuelve el tipo de animal para mostrar en el sistema
+     * @return descripción breve de que se trata de un mamífero
+     */
     @Override
     public String getTipoAnimal() {return "Mamífero registrado";
     }
 
+    /**
+     * Método toString para mostrar los datos completos del mamífero
+     * @return texto formateado con los atributos heredados y propios
+     */
     @Override
     public String toString() {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
